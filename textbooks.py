b16e27e672c03d3b0ec162c91ce284344c815132
Dekun Zhang <rsdking@126.com>
2020-03-26 13:11:56 +0800

    Create textbooks.py

---
+++ textbooks.py
@@ -0,0 +1,45 @@
+from bs4 import BeautifulSoup
+import bs4
+import requests
+import wget
+
+def getHTMLText(url):
+    try:
+        r = requests.get(url, timeout = 30)
+        r.raise_for_status()
+        r.encoding = r.apparent_encoding
+        return r.text
+    except:
+        return ""
+
+def fillPdfList(bkName, bkUrl, html):
+    soup = BeautifulSoup(html, "html.parser")
+    for div in soup.find("div", id="container").children:
+        if isinstance(div, bs4.element.Tag) and div.attrs['class'] == ["con_list_jcdzs2020"]:
+            for ul in div.children:
+                if isinstance(ul, bs4.element.Tag) and ul.attrs['class'] == ["clearfix"]:
+                    for li in ul.children:
+                        if isinstance(li, bs4.element.Tag):
+                            names = li('h6')
+                            links = li('a', title="下载")
+                            for i in names:
+                                bkName.append(i.string)
+                            for i in links:
+                                bkUrl.append("http://bp.pep.com.cn/jc/ptgzkcbzsyjks"+i.attrs['href'][1:])
+
+
+def downloadPdf(bkName, bkUrl):
+    for i in range(119, len(bkName)):
+        url = bkUrl[i]
+        wget.download(url, bkName[i]+".pdf")
+        '''print(i)
+        print(bkName[i])'''
+
+def main():
+    bkName = []
+    bkUrl = []
+    url = 'http://bp.pep.com.cn/jc/ptgzkcbzsyjks/'
+    html = getHTMLText(url)
+    fillPdfList(bkName, bkUrl, html)
+    downloadPdf(bkName, bkUrl)
+main()
